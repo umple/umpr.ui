47893f021d37be02aeee798f5648fa2b41ec3339
Kevin Brightwell <kevin.brightwell2@gmail.com>
2015-03-26 11:52:13 -0400

    BasicFilters: Adds basic filter support, aesthetics are coming
    * SASS is used for CSS now (http://sass-lang.com/guide), PhpStorm has file watchers to handle it automatically, otherwise commandline work too
    * Upgraded bootstrap & jQuery versions
    * Added lodash (currently unused, might remove it)

--- app/views/home.html.php
+++ app/views/home.html.php
@@ -41,52 +41,62 @@ array_unique($diagramTypes);
 
 <div class="container mtb">
 
-  <div class="form-inline">
-    <div class="col-md-3">
-      <div class="input-group">
-        <label for="filter-repository">Repository &nbsp;</label>
-        <select class="form-control" id="filter-repository">
-          <option></option>
-          <?php foreach ($repoNames as $name) { ?>
-            <option><?php echo $name ?></option>
-          <?php } ?>
-        </select>
-      </div>
-    </div>
-    &nbsp;
+  <div class="filter-group">
+    <div class="form-inline">
+      <div class="row">
+        <div class="col-md-4">
+          <label for="filter-repository">Repository &nbsp;</label>
+          <select class="input-control" id="filter-repository">
+            <option value="null"></option>
+            <?php foreach ($repoNames as $name) { ?>
+              <option><?= $name ?></option>
+            <?php } ?>
+          </select>
+        </div>
 
-    <div class="col-md-3">
-      <div class="input-group">
-        <label for="filter-diagram-type">Diagram Type &nbsp;</label>
-        <select class="form-control" id="filter-diagram-type">
-          <option></option>
-          <?php foreach ($diagramTypes as $dtype) { ?>
-            <option><?php echo $dtype ?></option>
-          <?php } ?>
-        </select>
-      </div>
-    </div>
-    &nbsp;
 
-    <div class="col-md-3">
-      <div class="input-group">
-        <label for="filter-name">Name &nbsp;</label>
-        <input type="email" class="form-control" id="filter-name" placeholder="Name..">
+        <div class="col-md-4">
+          <label for="filter-diagram-type">Diagram Type &nbsp;</label>
+          <select class="input-control" id="filter-diagram-type">
+            <option value="null"></option>
+            <?php foreach ($diagramTypes as $dtype) { ?>
+              <option value="<?= $dtype ?>"><?= ucfirst($dtype) ?></option>
+            <?php } ?>
+          </select>
+        </div>
       </div>
-    </div>
 
-    &nbsp;
+      <div class="row">
+        <div class="col-md-4">
+          <label for="filter-file-type">Input Type &nbsp;</label>
+          <select class="input-control" id="filter-file-type">
+            <option value="null"></option>
+            <?php foreach ($fileTypes as $type) { ?>
+              <option><?= $type ?></option>
+            <?php } ?>
+          </select>
+        </div>
 
-    <div class="col-md-3">
-      <div class="input-group">
-        <label for="filter-last-state">Failure State &nbsp;</label>
-        <select class="form-control" id="filter-last-state">
-          <option></option>
-          <?php foreach (array_keys($ImportStates) as $state) { ?>
-            <option><?php echo $state ?></option>
-          <?php } ?>
-        </select>
+        <div class="col-md-4">
+          <div class="input-group">
+            <label for="filter-name">Name &nbsp;</label>
+            <input type="text" class="input-control" id="filter-name" placeholder="Name..">
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="input-group">
+            <label for="filter-last-state">Failure State &nbsp;</label>
+            <select class="input-control" id="filter-last-state">
+              <option value="null"></option>
+              <?php foreach (array_keys($ImportStates) as $state) { ?>
+                <option><?= $state ?></option>
+              <?php } ?>
+            </select>
+          </div>
+        </div>
       </div>
+
     </div>
 
   </div>
@@ -110,22 +120,27 @@ array_unique($diagramTypes);
           <?php foreach ($repo["files"] as $file) {
             $folder = "./data/" . $repo["name"] . "/";
 
-            $idTag = str_replace(".", "-", $file["path"]);
+            $idTag = preg_replace("/\\./", "-", $file["path"]);
 
             ?>
-            <tr>
-              <td class="col-repo-name"><?php echo $repo["name"]; ?></td>
-              <td class="col-diagram-type"><?php echo $repo["diagramType"]; ?></td>
-              <td class="col-data-type"><?php echo $file["type"]; ?></td>
+            <tr class="info-import" id="row-<?= $idTag ?>"
+                data-repository="<?= $repo["name"] ?>"
+                data-diagram-type="<?= $repo["diagramType"] ?>"
+                data-input-type="<?= $file["type"] ?>"
+                data-name="<?= $file["path"] ?>"
+                data-last-state="<?= $file["lastState"] ?>">
+              <td class="col-repo"><?= $repo["name"]; ?></td>
+              <td class="col-diagram-type"><?= ucfirst($repo["diagramType"]) ?></td>
+              <td class="col-input-type"><?= $file["type"]; ?></td>
               <td class="col-name">
-                <?php echo $file["path"] ?>
+                <?= $file["path"] ?>
 
                 &nbsp;
 
                 <div style="float: right">
 
                 <?php if ($file["successful"] || $ImportStates[$file["lastState"]] > $ImportStates["Fetch"]) { ?>
-                  <a href="<?php echo $folder . $file["path"] ?>">(Source)</a>
+                  <a href="<?= $folder . $file["path"] ?>">(Source)</a>
                 <?php } else { ?>
                   <span class="text-warning" title="Unable to fetch source">(Source)</span>
                 <?php } ?>
@@ -133,7 +148,7 @@ array_unique($diagramTypes);
                 &nbsp;
 
                 <?php if ($file["successful"] || $ImportStates[$file["lastState"]] >= $ImportStates["Model"] ) { ?>
-                  <a href="<?php echo $folder . $file["path"] . ".ump" ?>">(Model)</a>
+                  <a href="<?= $folder . $file["path"] . ".ump" ?>">(Model)</a>
                 <?php } else { ?>
                   <span class="text-warning" title="Unable to import umple model">(Model)</span>
                 <?php } ?>
@@ -168,7 +183,7 @@ array_unique($diagramTypes);
 
             <?php // write the extra row:
             if (!$file["successful"]) { ?>
-                <tr class="error-info">
+                <tr class="info-error">
                   <td colspan="6" style="padding: 0 !important;">
                     <div class="accordian-body collapse" id="message-row-<?php echo $idTag ?>">
                       <pre><?php echo $file["message"] ?></pre>
