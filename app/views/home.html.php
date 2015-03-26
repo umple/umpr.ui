835ac588c51fdd8f409dbf44c31bf51647c900e8
Kevin Brightwell <kevin.brightwell2@gmail.com>
2015-03-26 00:55:40 -0400

    Filter: Add form controls, still layout issues.

--- app/views/home.html.php
+++ app/views/home.html.php
@@ -1,4 +1,96 @@
+
+<?php
+$jsonData = file_get_contents("data/meta.json");
+
+$data = json_decode($jsonData, true);
+
+//        echo "<pre>$jsonData</pre>";
+
+$umpleOnlineUrl = "http://cruise.eecs.uottawa.ca/umpleonline/?filename=";
+
+function unicodeString($str, $encoding=null) {
+  if (is_null($encoding)) $encoding = ini_get('mbstring.internal_encoding');
+  return preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/u', create_function('$match', 'return mb_convert_encoding(pack("H*", $match[1]), '.var_export($encoding, true).', "UTF-16BE");'), $str);
+}
+
+$ImportStates = array(
+    "Fetch"    => 0,
+    "Import"   => 1,
+    "Model"    => 2,
+    "Complete" => 3
+);
+
+$repoNames = [];
+$fileTypes = [];
+$diagramTypes = [];
+
+foreach ($data["repositories"] as $repo) {
+  array_push($repoNames, $repo["name"]);
+  array_push($diagramTypes, $repo["diagramType"]);
+
+  foreach ($repo["files"] as $file) {
+    array_push($fileTypes, $file["type"]);
+  }
+}
+
+array_unique($repoNames);
+array_unique($fileTypes);
+array_unique($diagramTypes);
+
+?>
+
 <div class="container mtb">
+
+  <div class="form-inline">
+    <div class="col-md-3">
+      <div class="input-group">
+        <label for="filter-repository">Repository &nbsp;</label>
+        <select class="form-control" id="filter-repository">
+          <option></option>
+          <?php foreach ($repoNames as $name) { ?>
+            <option><?php echo $name ?></option>
+          <?php } ?>
+        </select>
+      </div>
+    </div>
+    &nbsp;
+
+    <div class="col-md-3">
+      <div class="input-group">
+        <label for="filter-diagram-type">Diagram Type &nbsp;</label>
+        <select class="form-control" id="filter-diagram-type">
+          <option></option>
+          <?php foreach ($diagramTypes as $dtype) { ?>
+            <option><?php echo $dtype ?></option>
+          <?php } ?>
+        </select>
+      </div>
+    </div>
+    &nbsp;
+
+    <div class="col-md-3">
+      <div class="input-group">
+        <label for="filter-name">Name &nbsp;</label>
+        <input type="email" class="form-control" id="filter-name" placeholder="Name..">
+      </div>
+    </div>
+
+    &nbsp;
+
+    <div class="col-md-3">
+      <div class="input-group">
+        <label for="filter-last-state">Failure State &nbsp;</label>
+        <select class="form-control" id="filter-last-state">
+          <option></option>
+          <?php foreach (array_keys($ImportStates) as $state) { ?>
+            <option><?php echo $state ?></option>
+          <?php } ?>
+        </select>
+      </div>
+    </div>
+
+  </div>
+
   <div class="row">
     <!-- LEFT PANE -->
     <div class="col-lg-12">
@@ -13,29 +105,6 @@
           <th>Umple Online</th>
 
         </thead>
-
-        <?php
-        $jsonData = file_get_contents("data/meta.json");
-
-        $data = json_decode($jsonData, true);
-
-//        echo "<pre>$jsonData</pre>";
-
-        $umpleOnlineUrl = "http://cruise.eecs.uottawa.ca/umpleonline/?filename=";
-
-        function unicodeString($str, $encoding=null) {
-          if (is_null($encoding)) $encoding = ini_get('mbstring.internal_encoding');
-          return preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/u', create_function('$match', 'return mb_convert_encoding(pack("H*", $match[1]), '.var_export($encoding, true).', "UTF-16BE");'), $str);
-        }
-
-        $ImportStates = array(
-            "Fetch"    => 0,
-            "Import"   => 1,
-            "Model"    => 2,
-            "Complete" => 3
-        );
-
-        ?>
 
         <?php foreach ($data["repositories"] as $repo) { ?>
           <?php foreach ($repo["files"] as $file) {
