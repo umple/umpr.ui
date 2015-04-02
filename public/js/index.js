02a56aec3e362ba896354671a5a5e4d127faebfb
Kevin Brightwell <kevin.brightwell2@gmail.com>
2015-04-02 19:35:08 -0400

    Add URL parameters for filtering

--- public/js/index.js
+++ public/js/index.js
@@ -30,15 +30,18 @@
                 }
             }
 
+            var queryParams = {};
             tags.forEach(function (tag) {
+                var val = $('#filter-' + tag, $filterGroup).val();
                 if (tag !== 'name') {
-                    appendAttrSelector(tag, $('#filter-' + tag, $filterGroup).val());
+                    appendAttrSelector(tag, val);
                 } else {
-                    var name = $('#filter-name', $filterGroup).val();
-                    if (name !== '') {
-                        selector += '[data-name*=' + name + ']';
+                    if (val !== '') {
+                        selector += '[data-name*=' + val + ']';
                     }
                 }
+
+                queryParams[tag] = (_.isNull(val) || _.isUndefined(val) || val === "null") ? "" : val;
             });
 
             var $umprSummary = $('.umpr-summary');
@@ -50,8 +53,11 @@
             $toHide.hide();
             $toShow.show();
 
-            console.log('selector = ' + selector);
-            console.log('results =', $(selector));
+            console.log("queryParams =", queryParams);
+
+            if (shouldRunFilters) {
+                history.pushState({}, "", "?" + $.param(queryParams));
+            }
         }
 
         // Init code:
@@ -76,6 +82,37 @@
             shouldRunFilters = true;
             _runFilters();
         });
+
+        /**
+         * Load from the parameters in the URL => run on page load and history changes
+         * @private
+         */
+        function _loadFromParams() {
+            var match,
+                pl     = /\+/g,  // Regex for replacing addition symbol with a space
+                search = /([^&=]+)=?([^&]*)/g,
+                decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+                query  = window.location.search.substring(1);
+
+            var urlParams = {};
+            while (match = search.exec(query))
+                urlParams[decode(match[1])] = decode(match[2])
+
+            shouldRunFilters = false;
+            var $filterGroup = $('.filter-group');
+            _.each(urlParams, function (val, key) {
+                if (val !== "") {
+                    $('#filter-' + key, $filterGroup).val(val);
+                } else {
+                    $('#filter-' + key, $filterGroup).val(key === "name" ? "" : "null");
+                }
+            });
+            shouldRunFilters = true;
+            _runFilters();
+        }
+
+        $(window).bind('popstate', _loadFromParams());
+        $(window).bind('load',     _loadFromParams());
 
         /*
          * Expose modules
