3ea3ddbc57288e25942affb47df7f225c094ab2d
Kevin Brightwell <kevin.brightwell2@gmail.com>
2015-04-08 08:26:59 -0400

    Move around configuration settings.

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
 require_once("_config.php");
-$GLOBALS["viewables"]['view'] = 'home';
 
-$GLOBALS["viewables"]['title'] = "Umpr - Imported Umple Repository";
-setg('meta-description', 'Umpr Imported Umple Model Repository');
-setg('meta-keywords', 'umple,model,repository');
+setg('view', 'home');
+
+setg('title', "Umpr - Imported Umple Repository");
+
 
 // this should be the last line
 require_view('layout/wrapper');
