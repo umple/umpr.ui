57ad9618b8768839a2d4edc4a82a472604c5dd9f
Andrew Forward <aforward@gmail.com>
2015-04-08 08:15:18 -0400

    Moved the meta-* to index.php

--- public/_config.php
+++ public/_config.php
@@ -2,9 +2,6 @@
 session_start();
 require_once("_autoload.php");
 
-$GLOBALS["viewables"]["meta-description"] = "A repoistory of software models imported into Umple.";
-$GLOBALS["viewables"]["meta-keywords"] = "umple,model,repository";
-
 function require_view($viewName,$locals = array())
 {
   setg("locals",$locals);
--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ $GLOBALS["viewables"]['view'] = 'home';
 
 $GLOBALS["viewables"]['title'] = "Umpr - Imported Umple Repository";
 setg('meta-description', 'Umpr Imported Umple Model Repository');
+setg('meta-keywords', 'umple,model,repository');
 
 // this should be the last line
 require_view('layout/wrapper');
